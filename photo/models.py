6a218d73c5666bee055638e345349dc2f3768b71
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-02-15 18:01:49 -0600

    se agregaron las funcionaledades de ver las fotos

--- photo/models.py
+++ photo/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, null= False, blank=False)
+    class Meta:
+        ordering = ['name',]
+
 
     def __str__(self):
         return self.name
--- photo/views.py
+++ photo/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from .models import Category, Photo
 
 # Create your views here.
 
 def gallery(request):
-    return render (request, 'photos/gallery.html')
+    categorias = Category.objects.all()
+    fotos = Photo.objects.all()
+
+    context = {
+        'categorias': categorias,
+        'fotos': fotos,
+    }
+
+    return render (request, 'photos/gallery.html', context)
 
 def wiewPhoto(request, pk):
-    return render (request, 'photos/photo.html')
+    foto = Photo.objects.get(id=pk)
+    context = {
+        'foto': foto,
+    }
+
+    return render (request, 'photos/photo.html', context)
 
 def addPhoto(request):
     return render (request, 'photos/add.html')
