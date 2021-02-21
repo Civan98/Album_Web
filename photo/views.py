d84f6ca1980703c36481d090cf978b75e85cb2ec
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-02-20 23:47:01 -0600

    formulario

--- photo/views.py
+++ photo/views.py
@@ -23,4 +23,17 @@ def wiewPhoto(request, pk):
     return render (request, 'photos/photo.html', context)
 
 def addPhoto(request):
-    return render (request, 'photos/add.html')
+    categorias = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        print('data: ',data)
+        print('image: ',image)
+
+    context = {
+        'categorias': categorias,
+    }
+
+    return render (request, 'photos/add.html', context)
